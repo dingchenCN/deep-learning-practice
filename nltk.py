3826bd5ef17a59b6826c690c58b27cdd0b6b5e2c
dingchen_cn <dingchen.cn@gmail.com>
2017-07-04 23:05:44 +0800

    nltk

--- nltk.py
+++ nltk.py
@@ -26,3 +26,31 @@ fdist1['whale']
 
 fdist1.plot(50, cumulative=True)
 fdist1.hapaxes() # 只出现了一次的词（所谓的hapaxes）
+
+V = set(text1)
+long_words = [w for w in V if len(w) > 15]
+sorted(long_words)
+
+fdist5 = FreqDist(text5)
+sorted([w for w in set(text5) if len(w) > 7 and fdist5[w] > 7])
+
+bigrams(['more', 'is', 'said', 'than', 'done'])
+text4.collocations()
+
+[len(w) for w in text1]
+fdist = FreqDist([len(w) for w in text1])
+fdist.keys()
+fdist.items()
+fdist.max()
+fdist[3]
+fdist.freq(3)
+
+[w for w in sent7 if len(w) < 4]
+[w for w in sent7 if len(w) <= 4]
+
+sorted([w for w in set(text7) if '-' in w and 'index' in w])
+
+nltk.chat.chatbots()
+
+sorted([w for w in set(text5) if w.startswith('b')])
+
--- spacytest.py
+++ spacytest.py
@@ -49,3 +49,28 @@ hippos = doc[8]
 
 assert apples.similarity(oranges) >= boots.similarity(hippos)
 
+from spacy.parts_of_speech import ADV
+
+def is_adverb(token):
+    return token.pos == spacy.parts_of_speech.ADV
+
+# These are data-specific, so no constants are provided. You have to look
+# up the IDs from the StringStore.
+NNS = nlp.vocab.strings['NNS']
+NNPS = nlp.vocab.strings['NNPS']
+def is_plural_noun(token):
+    return token.tag == NNS or token.tag == NNPS
+
+def print_coarse_pos(token):
+    print(token.pos_)
+
+def print_fine_pos(token):
+    print(token.tag_)
+
+def dependency_labels_to_root(token):
+    '''Walk up the syntactic tree, collecting the arc labels.'''
+    dep_labels = []
+    while token.head is not token:
+        dep_labels.append(token.dep)
+        token = token.head
+    return dep_labels
